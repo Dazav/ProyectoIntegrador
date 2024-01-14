c1c651abaae6956a4a7540dae48142e419e7adb5
wei <miguelxu124@.gmail.com>
2024-01-14 17:34:38 +0100

    finish but not finished yet

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
+// presentar el título como mágina de escribir
 document.addEventListener("DOMContentLoaded", function() {
+    // conseguir id de título
     const h1 = document.getElementById('titulo');
+    // dividir el título en cada letra que incluir espacio
     h1.innerHTML = h1.textContent.replace(/./g,"<span>$&</span>");
+    // configurar delay inicial
     let delay =0;
     document.querySelectorAll('span').forEach(function(span,index){
+        // cada 0.05s escribir una letra
         delay+=0.05;
+        // cambiar el color del palabra "psicológico" en el caso que escribir este
         if (index>11 && index<24){
             span.style.setProperty('color', '#48A45A');
         }
+        // colocar propiedad al animación
         span.style.setProperty('--delay', delay+'s');
         console.log(delay);
     });
 });
+// animación de carta del recurso
 $(document).ready(function () {
+    // en el caso de clic la carta, cambiará la clase. 
     $(".carta1").on("click", function () {
         $(".carta1").toggleClass("active1");
     });
