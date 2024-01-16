9aa0e0769264e82cedec32f08d26f5a06b5c4f97
wei xu <miguelxu124@gmail.com>
2024-01-16 22:50:25 +0100

    commmmmmmmmmentario

--- js/logIn.js
+++ js/logIn.js
@@ -2,14 +2,18 @@
 $(document).ready(function () {
     $("#inputC").blur(function () { 
         if ($(this).val()!=='') {
+            //si no está vacio, va a agregar estilo css "active" en la palabra "email"
             $(".email").addClass("active");
         } else {
+            //si está vacio, va a quitar eso estilo css
             $(".email").removeClass("active");
         }
     });
     $("#inputC").focus(function (e) { 
+        //mientras enfoque el input, lo agregará tambien
         $(".email").addClass("active");        
     });
+    // debido a que la posición de "password" después de animación es ditinto que la de "email",tambien hace falta configurar este.
     $("#inputP").blur(function () { 
         if ($(this).val()!=='') {
             $(".pwd").addClass("active2");
@@ -24,18 +28,21 @@ $(document).ready(function () {
 // 
 $(document).ready(function () {
     // animacion de formulario
+    //configurar tiempo inicial
     var time=0;
+    // conseguir todas las clases que se llama btn
     document.querySelectorAll('.btn').forEach(function(btn){
-        // cada 0.05s escribir una letra
+        // se aparece el elemento más tarde 0.2 s que el anterior etc...
         time+=0.2;
-        // colocar propiedad al animación
+        // colocar time al animaction-delay
         btn.style.setProperty('--time', time+'s');
         console.log(time);
     });
     $(".continua").animate({opacity: 1},{duration: 1000});
     var time=0.5;
     document.querySelectorAll('button').forEach(function(button){
-        // cada 0.05s escribir una letra
+        // El primero bóton se aparece después de 0.5s
+        // luego se aparece cada uno más tarde 0.3 s que el anterior etc...
         time+=0.3;
         // colocar propiedad al animación
         button.style.setProperty('--time', time+'s');
