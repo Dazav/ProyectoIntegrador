9472ed3af8bc8d0beca0f33af14f0e35ca9f2ab9
wei <miguelxu124@.gmail.com>
2024-01-18 20:14:43 +0100

    ok

--- js/foro.js
+++ js/foro.js
@@ -5,3 +5,15 @@ $(document).ready(function () {
         b.style.setProperty("--delay", time+"s");
     });
 });
+// 
+$(document).ready(function () {
+    var agitar="bx-tada";
+    $("input").focus(function () { 
+        $("i").addClass(agitar);
+    }).blur(function () { 
+        $("i").removeClass(agitar);
+    });
+    $(selector).keypress(function (e) { 
+        
+    });
+});
