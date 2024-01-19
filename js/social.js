f673f9004345b3e909236bd20e4abee661207d52
wei <miguelxu124@.gmail.com>
2024-01-20 00:43:11 +0100

    ok

--- js/social.js
+++ js/social.js
@@ -1,3 +1,4 @@
+//la deferencia de tiempo de aparecer el título
 $(document).ready(function () {
     var time=0;
     document.querySelectorAll("b").forEach(function (b) {  
@@ -5,7 +6,7 @@ $(document).ready(function () {
         b.style.setProperty("--delay", time+"s");
     });
 });
-// 
+// animación hover de icon de buscador
 $(document).ready(function () {
     var agitar="bx-tada";
     $("input").mouseenter(function () { 
@@ -20,30 +21,58 @@ $(document).ready(function () {
     $(".tema").css({
         position: "relative",
         opacity:0,
-        bottom: "-100px",
+        bottom: "20px",
         right:"-50px",
+        transform: "rotateX(-20deg) rotateY(25deg)",
+        transformOrigin: "-20% -20%"
     });
+    //desplazarse el barra de deplazamiento del nevagador
     $(window).scroll(function () { 
         var scrollTop = $(document).scrollTop()
+        //
         var topTitulo=$(".forosTitulo").height()/3;
         var time=0;
         if (scrollTop > topTitulo) {
             $('.tema').each( function (index,tema) { 
-                time +=300;
+                time +=400;
                 $(tema).delay(time).animate({
                     opacity:1,
                     bottom: "0",
                     right:"0"
-                },{
-                    duration: 500
+                },500, function () {
+                    $(this).css({
+                        transformOrigin: "50% 0",
+                        // transform: "rotate(0)",
+                        transition: "0.4s ease-in-out"
+                    });
                 });
             }); 
         }
-    });        
+    });
+    //expandir la carta de tema por clic
+    $(".tema").on("click", function () {
+        $(this).css({
+            width: "50vw",
+            height: "100vh",
+            transform:  "rotate(0)",
+            transformOrigin: "0 0",
+            transition: "0.5s ease-in-out"
+        });
+        if ($(this).height()=="100vh") {
+            $(this).css({
+                width: "800px",
+                height: "200px",
+                transform: "rotateX(-20deg) rotateY(25deg)",
+                transformOrigin: "50% 0",
+                transition: "0.5s ease-in-out"
+            });
+        }
+    });
+
+        
+        // $(".tema").toggleClass("temaActive");       
 });
-
-
-//
+//buscador
 $(document).ready(function () {
     // buscar tema
     $("i").on("click", function () {
@@ -63,3 +92,4 @@ $(document).ready(function () {
         });
     });
 });
+
