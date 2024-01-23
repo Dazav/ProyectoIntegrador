8fe572367cf491545c48710f7077e7e6fd3e0623
ismaelmormor <ismaelmormor@gmail.com>
2024-01-23 07:59:08 +0100

    Base de datos hecha

--- ddbb/crear_tablas.php
+++ ddbb/crear_tablas.php
@@ -2,10 +2,10 @@
     include "conecta.php";
     // FUNCION PARA CREAR LAS TABLAS
     function crearTablas($conexion){
-        $tabla_usuario = "CREATE TABLE USUARIOS(
+        $tabla_usuario = "CREATE TABLE IF NOT EXISTS usuario(
             id int auto_increment primary key,
             nombre varchar(255),
-            password varchar(255),
+            pssword varchar(255),
             email varchar(255),
             nombreUser varchar(255),
             imagen varchar(255)
@@ -13,7 +13,7 @@
 
         mysqli_query($conexion, $tabla_usuario) or die("Error en tabla usuario");
 
-        $tabla_terapeuta = "CREATE TABLE terapeuta(
+        $tabla_terapeuta = "CREATE TABLE IF NOT EXISTS terapeuta(
             id INT AUTO_INCREMENT PRIMARY KEY,
             nombre VARCHAR(200),
             apellidos VARCHAR(200),
@@ -25,31 +25,32 @@
 
         mysqli_query($conexion, $tabla_terapeuta) or die("Error en tabla terapeuta");
 
-        $tabla_foro = "CREATE TABLE FORO(
+        $tabla_foro = "CREATE TABLE IF NOT EXISTS foro(
             id INT AUTO_INCREMENT PRIMARY KEY,
             id_usuario INT,
             titular VARCHAR(250),
             descripcion VARCHAR(250),
             img VARCHAR(100),
-            fecha_creacion DATE DEFAULT(CURDATE),
-            fecha_modificacion DATE DEFAULT(CURDATE),
+            fecha_creacion DATE DEFAULT CURRENT_TIMESTAMP,
+            fecha_modificacion DATE DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (id_usuario) REFERENCES usuario(id)
         );";
 
         mysqli_query($conexion, $tabla_foro) or die("Error en tabla usuario");
 
-        $tabla_respuestas = "CREATE TABLE respuestas(
+        $tabla_respuestas = "CREATE TABLE IF NOT EXISTS respuestas(
             id INT AUTO_INCREMENT PRIMARY KEY,
             id_usuario INT,
             respuesta VARCHAR(250),
-            fecha DATE DEFAULT(CURDATE),
+            fecha DATE DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (id_usuario) REFERENCES usuario(id)
         );";
 
         mysqli_query($conexion, $tabla_respuestas) or die("Error en tabla usuario");
 
-        $tabla_opiniones = "CREATE TABLE OPINIONES(
-            id int auto_increment primary key,
+        $tabla_opiniones = "CREATE TABLE IF NOT EXISTS opiniones(
+            id int AUTO_INCREMENT primary key,
+            id_autor INT,
             titulo varchar(255),
             descripcion varchar(255),
             foreign key (id_autor) references usuario(id)
@@ -57,7 +58,7 @@
 
         mysqli_query($conexion, $tabla_opiniones) or die("Error en tabla usuario");
 
-        $tabla_contacto = "CREATE TABLE CONTACTO(
+        $tabla_contacto = "CREATE TABLE IF NOT EXISTS contacto(
             id INT PRIMARY KEY AUTO_INCREMENT,
             nombre VARCHAR(100),
             email VARCHAR(200),
@@ -67,20 +68,20 @@
 
         mysqli_query($conexion, $tabla_contacto) or die("Error en tabla usuario");
 
-        $tabla_recursos = "CREATE TABLE recursos(
+        $tabla_recursos = "CREATE TABLE IF NOT EXISTS recursos(
             id INT AUTO_INCREMENT PRIMARY KEY,
             id_usuario INT,
             titular VARCHAR(250),
             descripcion VARCHAR(250),
             img VARCHAR(100),
-            fecha_creacion DATE DEFAULT(CURDATE),
-            fecha_modificacion DATE DEFAULT(CURDATE),
+            fecha_creacion DATE DEFAULT CURRENT_TIMESTAMP,
+            fecha_modificacion DATE DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (id_usuario) REFERENCES usuario(id)
         )";
 
         mysqli_query($conexion, $tabla_recursos) or die("Error en tabla usuario");
 
-        $tabla_gruposApoyo = "CREATE TABLE GruposApoyo(
+        $tabla_gruposApoyo = "CREATE TABLE IF NOT EXISTS GruposApoyo(
             id INT AUTO_INCREMENT PRIMARY KEY,
             organizador INT,
             idioma VARCHAR(250),
@@ -97,16 +98,16 @@
     // Conexio sin tener la base de datos
     $conexion = getConexionsindb();
     // Miramos is existe la base de datos
-    $result = mysqli_query($conexion, "SHOW DATABASES LIKE 'ambulatorio'");
+    $result = mysqli_query($conexion, "SHOW DATABASES LIKE 'brainhub'");
     // Si no existe, la creamos y creamos las tablas
     if (!$result->num_rows > 0){
-        $db = "CREATE DATABASE ambulatorio";
+        $db = "CREATE DATABASE brainhub";
         mysqli_query($conexion, $db)or die("Error al crear la database");
-        mysqli_select_db($conexion, "ambulatorio");
+        mysqli_select_db($conexion, "brainhub");
         crearTablas($conexion);
         
     }else{
         // Si ya existe, pasamos tambien el crear tablas por si acaso ya existiese pero sin contenido
-        mysqli_select_db($conexion, "ambulatorio");
+        mysqli_select_db($conexion, "brainhub");
         crearTablas($conexion);
     }
