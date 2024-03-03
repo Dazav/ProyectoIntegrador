5098c5ad289f8b89f637ffb8ec49a85be0df5d42
[Santiago Daza] <[santiagodaza22@gmail.com]>
2024-03-03 20:39:23 +0100

    editar perfil

--- html/perfil.php
+++ html/perfil.php
@@ -84,7 +84,7 @@ $resultado_citas = mysqli_query($conexion, $query_citas);
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <script src="../js/perfil.js"></script>
     <script src="../js/main.js"></script>
     <title>Perfil</title>
